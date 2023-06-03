56d938fb7db38c399fb2c16287b6a9035a0cc805
Airon <131386167+HaxHex04@users.noreply.github.com>
2023-06-03 23:54:20 +0100

    Return array reduced multiplying the values

---
+++ Return_reduce.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+    return x.reduce((a, b)=> a*b, 1)
+  }
+
+//   OR
+
+  function grow(x){
+   return x.reduce((acc , item) => acc*item )
+  }
