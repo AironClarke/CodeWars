1c7f1df485bacb6f4be39dbaf8a28e1ff0cb8452
Airon <131386167+HaxHex04@users.noreply.github.com>
2023-07-07 14:12:25 +0100

    Checks if elements are ascending

---
+++ 7 kyu/Sorted_yes_no_how.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {
+    return array.every((item,index) => index ==  0 || array[index - 1] < array[index]) ? "yes, ascending" :
+    array.every((item,index) => index == 0 || array[index - 1] > array[index]) ? "yes, descending" : "no"
+}
