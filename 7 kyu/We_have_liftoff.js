b6c79881feccf682246fb78dabb90ff476adcae0
Airon <131386167+HaxHex04@users.noreply.github.com>
2023-08-22 23:44:39 +0100

    concatenate the nth letter from each word

--- 7 kyu/We_have_liftoff.js
+++ 7 kyu/We_have_liftoff.js
@@ -1,5 +1,5 @@
 // DESCRIPTION:
-// You have an array of numbers 1 through n (where 1 <= n <= 10). The array needs to be formatted correctly for the person reading the countdown of a spaceship launch.
+// Y ou have an array of numbers1 through n (where 1 <= n <= 10). The array needs to be formatted correctly for the person reading the countdown of a spaceship launch.
 
 // Unfortunately, the person reading the countdown only knows how to read strings. After the array is sorted correctly make sure it's in a format he can understand.
 
